a4d678e57a4aaee16faccb1d82d12d074574070d
profanis <prodromouf@gmail.com>
2020-08-25 17:20:13 +0300

    select component in shared directory in the app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { SelectComponent } from './select/select.component';
+import { SelectComponent } from './shared/components/select/select.component';
+
 
 @NgModule({
   declarations: [
