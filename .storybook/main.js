25d4454f1cad9b5f7683b25e1ef02fd6d031a9aa
profanis <prodromouf@gmail.com>
2020-09-03 19:51:53 +0300

    default storybook setup

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../projects/ui-explorer/src/**/*.stories.mdx",
+    "../projects/ui-explorer/src/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
