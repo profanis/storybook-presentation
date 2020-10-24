702753685f670711406d85350167011d1ca11192
profanis <prodromouf@gmail.com>
2020-10-24 13:22:24 +0300

    global decorator for all stories

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -7,3 +7,14 @@ setCompodocJson(docJson);
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
 }
+
+// export const decorators = [
+//   (storyFunc) => {
+//     const story = storyFunc()
+
+//     return {
+//       ...story,
+//       template: `<div style="border: 1px solid #cccccc; border-radius:5px;  padding: 10px">${story.template}</div>`
+//     }
+//   }
+// ]
