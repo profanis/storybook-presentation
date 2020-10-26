3cf5eb48550a756c82fb3b1e0bd282bd4cd46e3d
profanis <prodromouf@gmail.com>
2020-10-26 19:49:41 +0200

    select component added in a module

--- projects/my-lib/src/public-api.ts
+++ projects/my-lib/src/public-api.ts
@@ -2,9 +2,11 @@
  * Public API Surface of my-lib
  */
 
-export * from './lib/components/select/select.component';
+export * from './lib/components/select/select.module';
+export * from './lib/components/select/select/select.component';
 export * from './lib/components/toggle/toggle.module';
 export * from './lib/components/toggle/toggle/toggle.component';
 export * from './lib/my-lib.module';
+
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { SelectComponent, ToggleModule } from '@my-lib';
+import { SelectModule, ToggleModule } from '@my-lib';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -8,12 +8,12 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
-    SelectComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ToggleModule
+    ToggleModule,
+    SelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
